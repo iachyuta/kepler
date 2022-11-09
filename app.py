171654ec78356604763cfc052309dace8baa31b6
bommaka.reddy@lntinfotech.com <bommaka.reddy@lntinfotech.com>
2022-11-09 05:28:41 +0000

    auto save by knights-watch

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return "<h1> I am in home page </h1>"
 
+@app.route('/about')
+def about():
+    return "<h1> About Me </h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
